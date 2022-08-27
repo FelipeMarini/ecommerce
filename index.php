1dfc3c63c9e8f12f8dc2c650cc1703125aae63bd
Felipe Marini <felipemarini71188@gmail.com>
2022-08-27 15:07:25 -0300

    Configurada a classe Page para templates

--- index.php
+++ index.php
@@ -2,22 +2,39 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+
+$app = new Slim();
 
 // mostra os erros explicados
 $app->config('debug', true);
 
-// rota principal
+
+// rota principal (home)
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
+    // teste para mostrar a conexão com o banco mysql e mostrar a tabela de users (formato JSON) na tela home
+	
+	// $sql = new Hcode\DB\Sql();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	// $results = $sql->select("SELECT * FROM tb_users");
 
-	echo json_encode($results);
+	// echo json_encode($results);
+
+	// construct chama o header
+	$page = new Page();
+
+	// método setTpl() chama o conteúdo (index)
+	$page->setTpl("index");
+
+	// no final o destruct chama o footer
 
 });
 
+
 $app->run();
+
 
  ?>
